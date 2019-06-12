032a17a9f28e398c992c81e2d9265c9b5ded9815
sujin03 <34879199+sujin03@users.noreply.github.com>
2019-06-12 16:01:14 +0900

    Add files via upload

--- project3/SplitArray.java
+++ project3/SplitArray.java
@@ -14,9 +14,10 @@
  * limitations under the License.
  */
 /**
- * Given an array full of integers positive or negative, write a method to move every negative number
- * to the left and every positive number to the right. Take into account that the order of these
- * elements among the same group(positive or negative) into the array doesn't care.
+ * Given an array full of integers positive or negative, write a method to move
+ * every negative number to the left and every positive number to the right.
+ * Take into account that the order of these elements among the same
+ * group(positive or negative) into the array doesn't care.
  *
  * Input: [1,2,3,-1,-2,-3] Output: [-2,-1,-3,2,3,1]
  *
@@ -24,120 +25,114 @@
  */
 public class SplitArray {
 
-	/**
-	 * The first solution implemented for this problem is based on the "Bubble Sort Algorithm".
-	 *
-	 * The time complexity of this solution is O(N^2) where N is number of elements in the array. It is even worse
-	 * than the bubble sort algorithm because to check if we have to swap between any pair of elements is really
-	 * expensive. The space complexity of this algorithm is O(1) because we are not
-	 * using any additional data structure.
-	 */
-	public void SplitSorting(int array[]) {
-		boolean flag = true;
-		while (flag)
-		{
-			flag = false;
-			for (int j = 0; j < array.length - 1; j++)
-			{
-				if (array[j] > array[j + 1])
-				{
-					Swap(array, j, j + 1);
-					// System.out.println("flag=" + flag);
-					flag = true;
-				}
-			}
-		}
-	}
+    /**
+     * The first solution implemented for this problem is based on the "Bubble Sort
+     * Algorithm".
+     *
+     * The time complexity of this solution is O(N^2) where N is number of elements
+     * in the array. It is even worse than the bubble sort algorithm because to
+     * check if we have to swap between any pair of elements is really expensive.
+     * The space complexity of this algorithm is O(1) because we are not using any
+     * additional data structure.
+     */
+    public void splitSorting(int array[]) {
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
+                    // System.out.println("flag=" + flag);
+                    flag = true;
+                }
+            }
+        }
+    }
 
-	/**
-	 * The second solution for this problem is much faster than the previous one. Here, instead of using a sorting
-	 * algorithm we are going over the array from left to right using two pointers and swapping
-	 * elements if needed. The time complexity of this algorithm is O(N) where N is the
-	 * number of elements in the array. The space complexity is also O(1) because we are not using any additional
-	 * data structure here as well.
-	 */
-	public void SplitSwappingIterative(int array[]) {
-		int left = 0;
-		int right = array.length - 1;
-		while (left < right)
-		{
-			boolean shouldChangeLeft = array[left] >= 0;
-			boolean shouldChangeRight = array[right] < 0;
-			if (shouldChangeLeft && shouldChangeRight)
-			{
-				Swap(array, left, right);
-				left++;
-				right--;
-			} else
-			{
-				if (!shouldChangeLeft)
-					left++;
-				else if (!shouldChangeRight)
-					right--;
-			}
-		}
-	}
+    /**
+     * The second solution for this problem is much faster than the previous one.
+     * Here, instead of using a sorting algorithm we are going over the array from
+     * left to right using two pointers and swapping elements if needed. The time
+     * complexity of this algorithm is O(N) where N is the number of elements in the
+     * array. The space complexity is also O(1) because we are not using any
+     * additional data structure here as well.
+     */
+    public void splitSwappingIterative(int array[]) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            boolean shouldChangeLeft = array[left] >= 0;
+            boolean shouldChangeRight = array[right] < 0;
+            if (shouldChangeLeft && shouldChangeRight) {
+                swap(array, left, right);
+                left++;
+                right--;
+            } else {
+                if (!shouldChangeLeft) {
+                    left++;
+                } else if (!shouldChangeRight) {
+                    right--;
+                }
+            }
+        }
+    }
 
-	/**
-	 * Using partition Method of Quicksort to split the array.
-	 * Its called Hoare's Partitioning Algorithm
-	 */
-	public void SplitSwappingPartition(int array[]) {
-		int left = 0;
-		int right = array.length - 1;
-		while (left < right)
-		{
-			while (array[left] < 0 && left < right)
-				left++;
-			while (array[right] >= 0 && left < right)
-				right--;
-			if (left < right)
-			{
-				Swap(array, left, right);
-				left++;
-				right--;
-			}
-		}
-	}
+    /**
+     * Using partition Method of Quicksort to split the array. Its called Hoare's
+     * Partitioning Algorithm
+     */
+    public void splitSwappingPartition(int array[]) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            while (array[left] < 0 && left < right)
+                left++;
+            while (array[right] >= 0 && left < right)
+                right--;
+            if (left < right) {
+                Swap(array, left, right);
+                left++;
+                right--;
+            }
+        }
+    }
 
-	/**
-	 * This is a tail recursive solution for this problem. This implementation has the same time complexity,
-	 * O(N). Here, the only change is how we are going to iterate over the array; in the previous
-	 * implementation we used a classic iterative approach whereas in this solution we are using
-	 * recursion to iterate. In this case, the space complexity is also O(1) because we are not using any additional data
-	 * structure.
-	 */
-	public void SplitSwappingRecursive(int array[]) {
-		if (array.length == 0)
-			return;
+    /**
+     * This is a tail recursive solution for this problem. This implementation has
+     * the same time complexity, O(N). Here, the only change is how we are going to
+     * iterate over the array; in the previous implementation we used a classic
+     * iterative approach whereas in this solution we are using recursion to
+     * iterate. In this case, the space complexity is also O(1) because we are not
+     * using any additional data structure.
+     */
+    public void splitSwappingRecursive(int array[]) {
+        if (array.length == 0)
+            return;
 
-		SplitSwappingRecursiveInner(array, 0, array.length - 1);
-	}
+        splitSwappingRecursiveInner(array, 0, array.length - 1);
+    }
 
-	private void SplitSwappingRecursiveInner(int array[], int left, int right) {
-		// System.out.println("left =" + left);
-		// System.out.println("right=" + right);
-		if (left < right)
-		{
-			boolean shouldChangeLeft = array[left] >= 0;
-			boolean shouldChangeRight = array[right] < 0;
-			if (shouldChangeLeft && shouldChangeRight)
-			{
-				Swap(array, left, right);
-				SplitSwappingRecursiveInner(array, left + 1, right - 1);
-			} else
-			{
-				if (!shouldChangeLeft)
-					SplitSwappingRecursiveInner(array, left + 1, right);
-				else if (!shouldChangeRight)
-					SplitSwappingRecursiveInner(array, left, right - 1);
-			}
-		}
-	}
+    private void splitSwappingRecursiveInner(int array[], int left, int right) {
+        // System.out.println("left =" + left);
+        // System.out.println("right=" + right);
+        if (left < right) {
+            boolean shouldChangeLeft = array[left] >= 0;
+            boolean shouldChangeRight = array[right] < 0;
+            if (shouldChangeLeft && shouldChangeRight) {
+                swap(array, left, right);
+                splitSwappingRecursiveInner(array, left + 1, right - 1);
+            } else {
+                if (!shouldChangeLeft)
+                    splitSwappingRecursiveInner(array, left + 1, right);
+                else if (!shouldChangeRight)
+                    splitSwappingRecursiveInner(array, left, right - 1);
+            }
+        }
+    }
 
-	private void Swap(int array[], int left, int right) {
-		int aux = array[right];
-		array[right] = array[left];
-		array[left] = aux;
-	}
+    private void swap(int array[], int left, int right) {
+        int aux = array[right];
+        array[right] = array[left];
+        array[left] = aux;
+    }
 }
